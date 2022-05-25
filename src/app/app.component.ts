d314aaa3178e838461763a000c20c4060bb1187e
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-05-25 19:48:08 +0200

    initial class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: "app-root",
-  templateUrl: "app.component.html"
-  
+  templateUrl: "app.component.html"  
 })
 
 export class AppComponent {
   titulo: string = "Contador App";
+  numero: number = 0;
+  base: number = 5;
+
+  operar(num: number) {
+    this.numero += num;
+  }
 }
