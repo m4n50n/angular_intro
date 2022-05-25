978ac1d4fe5394c4c953617582ab593493b91666
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-05-25 20:17:54 +0200

    modificacion de template

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -1,11 +1,20 @@
 import { Component } from "@angular/core";
 
 
-@Component ({
+@Component({
     selector: "app-heroe",
     templateUrl: "heroe.component.html"
 })
 
 export class HeroeComponent {
+    nombre: string = "Iron Man";
+    edad: number = 45;
 
+    get nombreCapitalizado() { /* Getter */
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${this.nombre} / Edad: ${this.edad} años.`
+    }
 }
