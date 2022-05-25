48fdd7bdebd9bce830a1b7245a61020d672c5cb0
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-05-25 18:49:58 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+/**
+ * selector: Nombre del componente (componente principal llamado en index.html)
+ * templateUrl: Path del template HTML del componente
+ * template: Permite definir un template HTML literal como por ejemplo:
+ *  template: `<span>Hola</span>`
+ */
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "app.component.html"
+  
 })
+
 export class AppComponent {
-  title = 'bases';
+  titulo: string = "Contador App";
 }
