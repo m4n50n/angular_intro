f2980f5477a43e9c9b39e2749df027f27bbee481
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-05-26 08:32:52 +0200

    change comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
  * selector: Nombre del componente (componente principal llamado en index.html)
  * templateUrl: Path del template HTML del componente
  * template: Permite definir un template HTML literal como por ejemplo:
- *  template: `<span>Hola</span>`
+ *    template: `<span>Hola</span>`
+ * styles: Estilos literales, como por ejemplo:
+ *    styles: ['h1 { font-weight: normal; }']
+ * styleUrls: Los estilos se aplican desde un fichero externo como por ejemplo:
+ *    styleUrls: ['./hero-app.component.css']
  */
 
 @Component({
