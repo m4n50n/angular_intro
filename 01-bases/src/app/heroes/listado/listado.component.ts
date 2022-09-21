3f66e778a6897914de56f38e4e475afe16e990af
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-09-21 16:43:00 +0200

    Directiva ngFor

--- 01-bases/src/app/heroes/listado/listado.component.ts
+++ 01-bases/src/app/heroes/listado/listado.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
 })
 
 export class ListadoComponent {
+  heroes: string[] = ["Spiderman", "Iron Man", "Thor", "Superman"];
+  heroeBorrado: string = "";
 
+  borrarHeroe():void {
+    console.log("Borrando...");    
+
+    // shift() elimina el primer elemento del array    
+    // Poniendo || "" significa que, en caso de obtener un undefined del shift() (en caso de que el array esté vacio, por ejemplo), retorne un string vacio ""
+    this.heroeBorrado = this.heroes.shift() || "";
+  }
 }
