c1ac84c61b7f69624417e75bc55c6a6c8f34bb80
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-10-13 20:41:08 +0200

    Componente para buscar por pais

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+  termino: string = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  buscar():void {
+    console.log(this.termino);
   }
-
 }
