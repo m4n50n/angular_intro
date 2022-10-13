c745d45caa976d068005444028aa133cefb4b32e
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-10-13 20:27:13 +0200

    Componente Sidebar

--- 03-paisesApp/src/app/shared/sidebar/sidebar.component.ts
+++ 03-paisesApp/src/app/shared/sidebar/sidebar.component.ts
@@ -4,6 +4,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: [
+    `
+      li { /* Este li SOLO se aplicará a los li de este componente y no a otros aunque pongamos el estilo con la tag genérica <li> */
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class SidebarComponent implements OnInit {
