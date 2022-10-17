2ca2e05ad51f535222b733d1f336628ad7fd711a
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2022-10-17 19:36:17 +0200

    Manejo de errores

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -9,16 +9,22 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
   termino: string = "";
+  hayError: boolean = false;
 
   constructor(private paisService: PaisService) {}
 
   buscar():void {
+    this.hayError = false;
     console.log(this.termino);
 
     // Nos suscribimos aquí y no en el servicio.
     this.paisService.buscarPais(this.termino)
-      .subscribe(respuesta => {
-        console.log(respuesta);
-      }); 
-  }
+    .subscribe((respuesta) => {
+      console.log(respuesta);
+    }, (error) => {
+      console.log("Error");
+      console.info(error);
+      this.hayError = true;
+    })
+  }    
 }
